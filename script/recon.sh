83e60df34f185bf72e6092e861fa9b2a0b0b1616
ssvevo <74205381+ssvevo@users.noreply.github.com>
2021-06-03 01:55:16 +0530

    Update recon.sh

--- script/recon.sh
+++ script/recon.sh
@@ -62,20 +62,22 @@ nuclei -l Recon/$1/$1-httprobe.txt -t exposed-panels -c 200 -H 'User-Agent: Mozi
 nuclei -l Recon/$1/$1-httprobe.txt -t takeovers -c 200 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36' -o Recon/$1/nuclei/$1-takeovers.txt | sleep 5
 
 echo -e "\e[1;34m\nRunning gau for $1 *** \n \e[0m"
-cat Recon/$1/$1-httprobe.txt | gau -o Recon/$1/$1-gau.txt
+echo "$1" | gauplus -b ttf,woff,svg,png,jpg -o Recon/$1/$1-gau.txt
 
 echo -e "\e[1;34m\nRunning grap JS.Json endpoint for $1 *** \n \e[0m"
 cat Recon/$1/$1-gau.txt |  grep -P "\w+\.js(\?|$)" | tee Recon/$1/$1-js.txt && cat Recon/$1/$1-gau.txt | grep -P "\w+\.txt(\?|$)" | tee Recon/$1/$1-txt.txt && cat Recon/$1/$1-gau.txt |  grep -P "\w+\.json(\?|$)" | tee Recon/$1/$1-json.txt
 
 rm -rf Recon/$1/$1-gau.txt
 
-echo -e "\e[1;35m\nRunning naabu for $1 *** \n \e[0m"
-cat  Recon/$1/$1-allDomain.txt |naabu -silent -o Recon/$1/$1-naabu.txt
+sleep 5
 
-echo -r "\e[1;35m\nRunning $1-naabulist httprobe for $1 *** \n \e[0m"
+echo -e "\e[1;35m\nRunning naabu for $1 *** \n \e[0m"
+naabu  -iL Recon/$1/$1-allDomain.txt -silent -warm-up-time 4 -top-ports 10000 -verify -o Recon/$1/$1-naabu.txt
+
+echo -e "\e[1;35m\nRunning $1-naabulist httprobe for $1 *** \n \e[0m"
 cat Recon/$1/$1-naabu.txt | httprobe -c 200 > Recon/$1/$1-naabu-httprobe.txt
 
-echo -r "\e[1;35m\nRunning $1-naabulist nuclei for $1 *** \n \e[0m"
+echo -e "\e[1;35m\nRunning $1-naabulist nuclei for $1 *** \n \e[0m"
 nuclei -l Recon/$1/$1-naabu-httprobe.txt -t cves -c 200 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36' -o Recon/$1/naabu-nuclei/$1-cves.txt | sleep 5
 nuclei -l Recon/$1/$1-naabu-httprobe.txt -t exposures -c 200 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36' -o Recon/$1/naabu-nuclei/$1-exposures .txt | sleep 5
 nuclei -l Recon/$1/$1-naabu-httprobe.txt -t misconfiguration -c 200 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36' -o Recon/$1/naabu-nuclei/$1-misconfiguration.txt | sleep 5
@@ -85,5 +87,5 @@ nuclei -l Recon/$1/$1-naabu-httprobe.txt -t default-logins -c 200 -H 'User-Agent
 nuclei -l Recon/$1/$1-naabu-httprobe.txt -t exposed-panels -c 200 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36' -o Recon/$1/naabu-nuclei/$1-exposed-panels.txt | sleep 5
 nuclei -l Recon/$1/$1-naabu-httprobe.txt -t takeovers -c 200 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36' -o Recon/$1/naabu-nuclei/$1-takeovers.txt | sleep 5
 
-echo -r "\e[1;35m\nRunning Subdoamin Takeover for $1 *** \n \e[0m"
-python3 ~/tools/subdover/subdover.py -t 200 -l Recon/$1/$1-allDomain.txt -o Recon/$1/$1-subdomain-takeover.txt
+echo -e "\e[1;35m\nRunning Subdoamin Takeover for $1 *** \n \e[0m"
+python3 ~/Tools/subdover/subdover.py -t 200 -l Recon/$1/$1-allDomain.txt -o Recon/$1/$1-subdomain-takeover.txt
