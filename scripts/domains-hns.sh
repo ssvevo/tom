8e83f1110337033317b29e72b1366345d3a64789
ssvevo <74205381+ssvevo@users.noreply.github.com>
2021-06-03 02:22:41 +0530

    Add files via upload

---
+++ scripts/domains-hns.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Let's Go!"
+
+subfinder -d $1 -silent -o $1-sub-domains.txt
+cat $1-sub-domains.txt | httpx -silent -o http-$1
+
+bash ~/nuclei.sh http-$1 | notify
+
+sleep 5
+
+cat $1-sub-domains.txt |naabu -silent -o  naabu-domains
+
+cat naabu-domains | httprobe -c 200 > naabu-$1
+
+bash ~/nuclei.sh naabu-$1 | notify
+
+sleep 5
+
+python3 ~/Tools/subdover/subdover.py -t 100 -l http-$1 -o $1-subdomain-takeover.txt
+
